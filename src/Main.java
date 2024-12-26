4528ac07b601bc24decf25de41cd8c99d815c9eb
Valeriya Mishurova <tvoyadenisova@yandex.ru>
2024-12-27 01:03:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 25_000;
+        int oneBonus = 20;
+        int Bonus = amount / oneBonus;
+
+        System.out.println("Количество начисленных миль: " + Bonus);
+    }
+}
